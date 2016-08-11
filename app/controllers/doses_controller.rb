6cd5aa3d31214b89c21f12c6035fa90e64ea3cd1
Guillaume Cochennec <guillaume.cochennec@outlook.fr>
2016-08-11 14:21:18 +0200

    affichage d'ingrédients dans la viex cocktail

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,18 @@
 class DosesController < ApplicationController
+
+   def new
+    @dose = @cocktails.doses.build
+   end
+
+   def create
+    @dose = Dose.new(review_params)
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+
 end
