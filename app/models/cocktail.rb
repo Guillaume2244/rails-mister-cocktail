a7c003ddcf52913b78e1486b16db5594be021614
Guillaume Cochennec <guillaume.cochennec@outlook.fr>
2016-08-11 11:42:47 +0200

    some adjustments on the Cocktail model

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,4 +1,8 @@
 class Cocktail < ApplicationRecord
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
+
   has_many :ingredients, through: :doses
-  has_many :doses
+  has_many :doses, dependent: :destroy
 end
--- db/migrate/20160811090221_create_doses.rb
+++ db/migrate/20160811090221_create_doses.rb
@@ -2,8 +2,8 @@ class CreateDoses < ActiveRecord::Migration[5.0]
   def change
     create_table :doses do |t|
       t.string :description
-      t.references :coktail, foreign_key: true
-      t.references :ingredient, foreign_key: true
+      t.references :cocktails, foreign_key: true
+      t.references :ingredients, foreign_key: true
 
       t.timestamps
     end
