a8eed9780caa7480a9cf714250933cf1e4441936
Guillaume Cochennec <guillaume.cochennec@outlook.fr>
2016-08-11 16:06:37 +0200

    all work

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,18 +1,31 @@
 class DosesController < ApplicationController
 
+    before_action :find_cocktail
+
    def new
-    @dose = @cocktails.doses.build
+    @dose = Dose.new
+    @ingredients = Ingredient.all
    end
 
    def create
-    @dose = Dose.new(review_params)
-    @review.restaurant = @restaurant
-    if @review.save
-      redirect_to restaurant_path(@restaurant)
+    @dose = @cocktail.doses.build(dose_params)
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
     else
       render :new
     end
   end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
 
 
 end
